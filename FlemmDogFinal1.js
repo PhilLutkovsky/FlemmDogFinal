20271352cafd738fc47a5238c0c5818158145a65
PhilLutkovsky <7plutkovsky@stpatrick.org>
2016-12-12 13:37:12 -0600

    Item Class
    Plus Console.log

---
+++ FlemmDogFinal1.js
@@ -0,0 +1,14 @@
+class Item{
+  constructor(n,p,s){
+  this.name = n;
+  this.price = p; //dollars
+  this.shipping = s; //days
+
+  }
+}
+
+var skateboard = new Item ("Toys R Us", 19.99, 3);
+var giftCard = new Item ("Itunes", 50, 1);
+var laptop = new Item ("Dell", 1200, 7);
+
+console.log ("Merry Christmas!")
