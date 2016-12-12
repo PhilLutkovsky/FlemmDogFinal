3b0521f9cf5514a1794747253e5aac8474de5617
PhilLutkovsky <7plutkovsky@stpatrick.org>
2016-12-12 13:58:13 -0600

    Unfinished CartClass
    finish subCart function

---
+++ CartClass.js
@@ -0,0 +1,31 @@
+
+class Cart{
+  constructor(i,q){
+  this.itemList = i;
+  this.itemQuantity = q;
+  }
+
+  addItem(i,q){
+    itemList.push(i);
+    itemQuantity.push(q);
+  }
+
+  totalCart(){
+    var total = 0;
+
+    for(t=0; t<this.itemList.length; t++);
+      total += (this.itemList[t]*this.q[t]);
+  }
+    return total;
+
+  subCart(days){
+    var subCart = new Cart();
+
+    for(t=0; t<list.itemList.length; t++){
+      if(this.itemList.shipping == )
+    }
+
+
+
+  }
+}
