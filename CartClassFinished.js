1063ce2e468334676e57ff820c09e62cd678a85d
PhilLutkovsky <7plutkovsky@stpatrick.org>
2016-12-13 13:38:44 -0600

    Finished Cart Class

---
+++ CartClassFinished.js
@@ -0,0 +1,32 @@
+
+class Cart{
+  constructor(i,q){
+  this.itemList = i;
+  this.itemQuantity = q;
+  }
+
+  addItem(i,q){
+    itemList.push(i);
+    itemQuantity.push(q);
+  }
+
+  totalCart(){
+    var total = 0;
+
+    for(let t=0; t<this.itemList.length; t++);
+      total += (this.itemList[t]*this.itemQuantity[t]);
+  }
+    return total;
+
+  subCart(days){
+    var subCart = new Cart([],[]);
+
+    for(let t=0; t<list.itemList.length; t++){
+      if(this.itemList[t].shipping == days){
+        subCart.itemList.push(this.itemList[t]);
+        subCart.itemQuantity.push(this.itemQuantity[t]);
+      }
+    }
+      return subCart;
+  }
+}
